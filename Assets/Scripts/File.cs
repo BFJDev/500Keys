a644308ab380b7315bbd92d7d98be77f59c20210
Ben <bfjdevmail@gmail.com>
2021-01-30 14:32:45 -0500

    Files can be dragged around the screen.

---
+++ Assets/Scripts/File.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class File : Draggable, IPointerUpHandler
+{
+    [SerializeField]
+    private Text _FileNameDisplay;
+
+    [SerializeField]
+    private string _FileName;
+
+    [SerializeField]
+    private Sprite _Image;
+
+    [SerializeField]
+    private string _Text;
+
+    private Vector2 _LastMousePosition;
+ 
+    void Start()
+    {
+        _FileNameDisplay.text = _FileName;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+    /*    foreach( GameObject hovered in eventData.hovered)
+        {
+            Overlappable overlappedObject = hovered.GetComponent<Overlappable>();
+            if(overlappedObject != null)
+            {
+                overlappedObject.HandleOverlappingFileReleased(this);
+                break;
+            }
+        }
+*/
+    }
+
+
+
+}
---
+++ Assets/Scripts/Overlappable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Overlappable : MonoBehaviour
+{
+    public abstract void FileOverlapStarted();
+
+    public abstract void FileOverlapEnded();
+
+    public abstract void HandleOverlappingFileReleased(File file);
+}

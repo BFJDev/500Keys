e5af39be2763c6979940898cc024104d7c0d07b9
Ben <bfjdevmail@gmail.com>
2021-01-30 18:06:02 -0500

    Overlappable Objects now work

--- Assets/Scripts/Overlappable.cs
+++ Assets/Scripts/Overlappable.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public abstract class Overlappable : MonoBehaviour
+public abstract class Overlappable : MonoBehaviour, IDropHandler
 {
     public abstract void FileOverlapStarted();
 
     public abstract void FileOverlapEnded();
 
     public abstract void HandleOverlappingFileReleased(File file);
+
+    public void OnDrop(PointerEventData data)
+    {
+        File file = data.pointerDrag.GetComponent<File>();
+        if(file != null)
+        {
+            file.gameObject.SetActive(false);
+        }
+    }
 }
